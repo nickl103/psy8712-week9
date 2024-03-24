f99403823bbd621d80bafdc1e80fea6b6fbb8fe7
nickl103 <nickl103@umn.edu>
2024-03-24 18:01:01 -0500

    attempting loop

--- R/cnbc_scraped.R
+++ R/cnbc_scraped.R
@@ -4,16 +4,30 @@ library(tidyverse)
 library(rvest)
 
 # Data Import and Cleaning
-urls <- c(Business = "https://www.cnbc.com/business/",
-          Investing = "https://www.cnbc.com/investing/",
-          Tech = "https://www.cnbc.com/technology/",
-          Politics= "https://www.cnbc.com/politics/")
-#savings urls and headers for later use in the loop 
+sections <- c(Business="https://www.cnbc.com/business/",
+          Investing="https://www.cnbc.com/investing/",
+          Tech= "https://www.cnbc.com/technology/",
+         Politics= "https://www.cnbc.com/politics/")
+
+
+#savings urls and section headers for later use in the loop 
 
 #making an empty tibble to put into the loop later
 cbnc_tbl <- tibble(headline= character(),
                      length= integer(),
                      source= character())
+for(i in 1: length(sections)){
+  url <- sections[i]
+  pages <- read_html(url) #reading in html pages
+  elements <- html_elements(pages,".Card-title") #extracting elements
+  text <- html_text(elements) #converting elements to text
+  length <- str_count(text, "\\S+") # counting number of words for string
+  source <- sections[i]
+  tbl <- tibble(headline= elements,
+                legnth= length,
+                source= source)
+  cnbc_tbl <- rbind(cnbc_tbl, tbl)
+}
 # Visualization
 
 # Data Analysis
