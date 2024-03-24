0f9eb8e4180c8d6da51e7780c6aece69e42f0e36
nickl103 <nickl103@umn.edu>
2024-03-24 16:01:41 -0500

    data import for rstats_tbl

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -2,3 +2,11 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(RedditExtractoR)
+
+# Data Import and Cleaning
+urls <- find_thread_urls(subreddit='rstats', period='month') #using find_thread_urls to get the specfic subreddit requested and to filter from the posts for at least a month's worth of posts. 
+thread <- get_thread_content(urls$url) #using get_thread_urls to create dataframe of data and comments from URL
+
+rstats_tbl <- tibble(post= thread$threads$title,
+                     upvotes = thread$threads$upvotes,
+                     comments= thread$threads$comments)
