6b7d671633f4ce4f09c094f6c87b52f488274194
nickl103 <nickl103@umn.edu>
2024-03-24 18:46:33 -0500

    publciation section for cnbc scraped and finishing explanations.

--- R/cnbc_scraped.R
+++ R/cnbc_scraped.R
@@ -10,7 +10,7 @@ urls <- c("https://www.cnbc.com/business/",
           "https://www.cnbc.com/politics/")
 sections <- c("Business","Investing","Tech","Politics")
 
-#savings urls and section headers for later use in the loop 
+#savings urls and section headers for later use in the loop, couldn't figure out how to make the for loop work if urls and sections were together. 
 
 #making an empty tibble to put into the loop later because when I tried to do it in the loop it wouldn't work properly and was only giving me like 30 observations????
 cnbc_tbl <- tibble(headline= character(),
@@ -27,10 +27,29 @@ for(u in 1:length(sections)) { #creating for loop for the four sections
   tbl <- tibble(headline = headline_nodes, 
                   length = length,
                   source = source) ##creating a tibble to combine with the other tibble
-  cnbc_tbl <- rbind(cnbc_tbl, tbl) 
+  cnbc_tbl <- rbind(cnbc_tbl, tbl)  #combining tbls
 }
 # Visualization
+#created a boxplot to show the length across each source
+cnbc_tbl %>% ggplot(aes(source, length)) +
+  geom_boxplot(fill="lightpink") +
+  labs(title= "Boxplot of Length and Source")
 
 # Data Analysis
+aov <- aov(cnbc_tbl$length ~ cnbc_tbl$source) #running aov as suggested
+aov_summary <- summary(aov) #creating summary table to later extract values for publication section
 
+
+p.value <- str_remove(formatC(aov_summary[[1]]$Pr[1], format="f", digits=2), "^0") #extracting p.value from anova summary and formating it as required
+F <- str_remove(formatC(aov_summary[[1]]$`F value`[1], format="f", digits=2), "^0") #extracting F from anova summary and formating it as 
+dfn <- aov_summary[[1]]$Df[1] #extracting dfn from anova summary 
+dfd <- aov_summary[[1]]$Df[2] #extracting dfd from anova summary
 # Publication
+
+#creating an ifelse test for was/was not if p.value is significant 
+was <- ifelse(p.value < .05, "was","was not")
+
+#creating the message using sprintf because it returns a character vector
+text <- sprintf("The results of an ANOVA comparing lengths across sources F(%d,%d) = %s,p = %s. This test %s statistically significant.",dfn,dfd,F, p.value, was)
+cat(text)
+#used the cat function to print the text out. 
