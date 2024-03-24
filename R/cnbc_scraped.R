5c92a3099bd6eee2816bfa292f265b45ec2b9de3
nickl103 <nickl103@umn.edu>
2024-03-24 18:16:45 -0500

    for loop redone because previous code was not working properly

--- R/cnbc_scraped.R
+++ R/cnbc_scraped.R
@@ -4,29 +4,30 @@ library(tidyverse)
 library(rvest)
 
 # Data Import and Cleaning
-sections <- c(Business="https://www.cnbc.com/business/",
-          Investing="https://www.cnbc.com/investing/",
-          Tech= "https://www.cnbc.com/technology/",
-         Politics= "https://www.cnbc.com/politics/")
-
+urls <- c("https://www.cnbc.com/business/",
+          "https://www.cnbc.com/investing/",
+          "https://www.cnbc.com/technology/",
+          "https://www.cnbc.com/politics/")
+sections <- c("Business","Investing","Tech","Politics")
 
 #savings urls and section headers for later use in the loop 
 
-#making an empty tibble to put into the loop later
-cbnc_tbl <- tibble(headline= character(),
+#making an empty tibble to put into the loop later because when I tried to do it in the loop it wouldn't work properly and was only giving me like 30 observations????
+cnbc_tbl <- tibble(headline= character(),
                      length= integer(),
                      source= character())
-for(i in 1: length(sections)){
-  url <- sections[i]
-  pages <- read_html(url) #reading in html pages
-  elements <- html_elements(pages,".Card-title") #extracting elements
-  text <- html_text(elements) #converting elements to text
-  length <- str_count(text, "\\S+") # counting number of words for string
-  source <- sections[i]
-  tbl <- tibble(headline= elements,
-                legnth= length,
-                source= source)
-  cnbc_tbl <- rbind(cnbc_tbl, tbl)
+
+for(u in 1:length(sections)) { #creating for loop for the four sections
+  page <- read_html(urls[u]) #reading in urls
+  headline_nodes <- page %>% #reading in the headlines
+    html_nodes(".Card-title") %>%
+    html_text(trim = TRUE) #converting to text, I tried to combine this with previous line with only one command but it wouldn't convert it properly to text
+  length <- str_count(headline_nodes, "\\S+") #counting words in each string
+  source <- sections[u] #creating a column for which section was source of urls
+  tbl <- tibble(headline = headline_nodes, 
+                  length = length,
+                  source = source) ##creating a tibble to combine with the other tibble
+  cnbc_tbl <- rbind(cnbc_tbl, tbl) 
 }
 # Visualization
 
